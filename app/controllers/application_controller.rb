72b650c081f749fcbc6148206c87f91232cb57e3
Chloé P <chloepuck13@yahoo.com>
2018-08-02 19:58:07 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,7 +36,8 @@ class MyApp < Sinatra::Base
     if @user_ans == 13110
       dogName="Cockapoo."
       welcome ="Thank you for taking our quiz, #{@user_name}. <br> <b> #{dogName}</a> </b> is the dog we've decided will works best for you!"
-      descrip="INSERT DESCRIPTION FOR COCKAPOO" img="http://www.ozcockapoos.net/ginger3.JPG”
+      descrip="INSERT DESCRIPTION FOR COCKAPOO" 
+      img="http://www.ozcockapoos.net/ginger3.JPG”
       url="https://en.wikipedia.org/wiki/Cockapoo"
 
     elsif @user_ans == 23110
