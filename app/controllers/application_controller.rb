b54b8efc8dbb31d7bea482c9e669c9023e4c6282
Chloé P <chloepuck13@yahoo.com>
2018-08-02 21:45:17 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -136,7 +136,7 @@ Bundler.require
     elsif @user_ans == 34210
       dogName="Cockapoo."
       welcome ="Thank you for taking our quiz, #{@user_name}. <br> <b> #{dogName}</a> </b> is the dog we've decided will works best for you!"
-      descrip="INSERT DESCRIPTION FOR COCKAPOO" 
+      descrip="v" 
       img="http://www.ozcockapoos.net/ginger3.JPG"
       url="https://en.wikipedia.org/wiki/Cockapoo"
 
