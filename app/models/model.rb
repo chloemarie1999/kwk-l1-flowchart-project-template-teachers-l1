30a55613cc5ba4eff4081f274cf61dd2f897bc41
Chloé P <chloepuck13@yahoo.com>
2018-08-02 21:10:10 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,8 +1,8 @@
-def total(value1, value2, value3)
+def total(value1, value2, value3, value4)
     value1=value1.to_i
     value2=value2.to_i
     value3=value3.to_i
     value4=value4.to_i
-    total=value1+value2+value3
+    total=value1+value2+value3+value4
 end
 
