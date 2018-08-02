4abd51a071be7e54022764b59f404a3df01977bb
Chloé P <chloepuck13@yahoo.com>
2018-08-02 21:08:32 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 # class ApplicationController < Sinatra::Base
 
-  # configure do
-  #   set :public_folder, 'public'
-  #   set :views, 'app/views'
-  # end
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
   
   require 'bundler'
 Bundler.require
